d786595c0be857532e66730b19c95b63555fb555
Ralf Hoffmann-Elbern <ralf.hoffmann@gmx.de>
2025-09-26 23:35:51 +0200

    Changed Bring to Front

--- AiHelper/MainWindow.xaml.cs
+++ AiHelper/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -30,7 +32,7 @@ namespace AiHelper
             this.PreviewKeyDown += MainWindow_PreviewKeyDown;
 
             Loaded += MainWindow_Loaded;
-           
+
         }
 
         private void Outputs_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
@@ -42,7 +44,7 @@ namespace AiHelper
 
             Task.Run(async () =>
             {
-            await Task.Delay(200);
+                await Task.Delay(200);
 
                 Dispatcher.Invoke(() =>
                 {
@@ -54,9 +56,6 @@ namespace AiHelper
                     catch { }
                 });
             });
-
-
-            //this.OutputDisplay.ScrollIntoView(this.viewModel.Outputs.Last());
         }
 
         private bool ShowAsDialog(Window window)
@@ -65,17 +64,52 @@ namespace AiHelper
             var result = window.ShowDialog();
             return result != null && result.Value == true;
         }
+        private nint hwnd;
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             this.SpaceButton.Focus();
             this.viewModel.Initialize();
+
+            hwnd = new WindowInteropHelper(this).EnsureHandle();
         }
 
         private void BringToTop()
         {
-            Dispatcher.Invoke(this.Activate);
+            Dispatcher.Invoke(async () =>
+            {
+                var hwndForground = GetForegroundWindow();
+                if (hwnd == hwndForground)
+                {
+                    return;
+                }
+
+                var threadId1 = GetWindowThreadProcessId(hwndForground, IntPtr.Zero);
+                var threadId2 = GetWindowThreadProcessId(hwnd, IntPtr.Zero);
+
+                if (threadId1 != threadId2)
+                {
+                    AttachThreadInput(threadId1, threadId2, true);
+                    SetForegroundWindow(hwnd);
+                    AttachThreadInput(threadId1, threadId2, false);
+                }
+                else
+                    SetForegroundWindow(hwnd);
+
+            });
         }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
 
         private async void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
         {
